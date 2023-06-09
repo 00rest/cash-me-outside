7dced91319d1a914398463b9f9752ee5174cb42a
JordAnne <jbethune37@gmail.com>
2023-06-08 22:12:17 -0400

    added back files that were deleted for JWT files :(

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Transaction = require('./User')
 
-module.exports = { User };
+module.exports = { User, Transaction };
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -2,26 +2,33 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type User {
-    _snn: SNN
+    _id: ID
     name: String
     email: String
     password: String
+    snn: Number
   }
+type Transaction {
+    _id: ID
+    transactionInfo: String
+    transactionUser: String
+}
 
 type Auth {
-    token: SNN
+    token: ID!
     user: User
   }
 
 type Query {
-    users: [User]!
-    user:(userSnn: SNN!): User
+    users: [User]
+    user(name: String!): User
+    transaction(name: String): [Transaction]
+    transaction(transactionId: ID!): Transaction
 }
 
-  type Mutation {
-addUser(name: String!, email: String!, password: String!): Auth
+type Mutation {
+addUser(name: String!, email: String!, password: String!, snn: Number!): Auth
 login(email: String!, password: String!): Auth
+addTransaction(transactionInfo: String!): Transaction
 }
 `;
-
-module.exports = typeDefs;
